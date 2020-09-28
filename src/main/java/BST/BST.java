2928494f40ead9a9e0e64353b4a49170816d5d62
shanmingda <shanmingda@yunshuxie.com>
2020-09-28 15:07:47 +0800

    二分搜索树的实现

--- src/main/java/BST/BST.java
+++ src/main/java/BST/BST.java
@@ -1,10 +1,51 @@
 package BST;
 
 /**
- * TODO
+ * 二分搜索树
+ * 存储的元素必须要有可比较性 所以要继承Comparable接口
  *
  * @author shanmingda
  * @date 2020-09-28 14:48
  */
-public class BST {
+public class BST<E extends Comparable<E>> {
+
+    /**
+     * 之所以要将节点类创建为私有内部类
+     * 是因为 二分搜索树的节点类的实现对用户来说没有必要可见
+     */
+    private class Node {
+
+        public E e;
+
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+    }
+
+    /**
+     * 根节点
+     */
+    private Node root;
+
+    /**
+     * 存储了多少个元素
+     */
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 }
