40ffc8596e301b0ca6352f59fca44f9cfdf1cfce
shanmingda <shanmingda@yunshuxie.com>
2020-09-28 17:26:12 +0800

    二分搜索树添加节点方法

--- src/main/java/BST/BST.java
+++ src/main/java/BST/BST.java
@@ -48,4 +48,36 @@ public class BST<E extends Comparable<E>> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    /**
+     * 向二分搜索树中添加新的元素e
+     * @param e
+     */
+    public void add(E e) {
+
+       root = add(root, e);
+    }
+
+    /**
+     * 向以node为根的二分搜索树中插入元素E 递归算法
+     * 返回插入新节点后二分搜索树的根
+     * @param node
+     * @param e
+     */
+    private Node add(Node node, E e) {
+        // 递归终止条件
+
+        if (node == null) {
+            size ++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(e) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
 }
